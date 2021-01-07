80323d16d545ab0f6da0ce5d52dd0194ffeb1ca8
Jared Forsyth <jared@jaredforsyth.com>
2021-01-07 10:25:14 -0600

    trims

--- client.js
+++ client.js
@@ -11,10 +11,10 @@ if (
 
 var Twitter = require('twitter-lite');
 var client = new Twitter({
-    consumer_key: process.env.API_KEY,
-    consumer_secret: process.env.API_SECRET,
-    access_token_key: process.env.USER_TOKEN,
-    access_token_secret: process.env.USER_SECRET,
+    consumer_key: process.env.API_KEY.trim(),
+    consumer_secret: process.env.API_SECRET.trim(),
+    access_token_key: process.env.USER_TOKEN.trim(),
+    access_token_secret: process.env.USER_SECRET.trim(),
 });
 
 module.exports = client;
